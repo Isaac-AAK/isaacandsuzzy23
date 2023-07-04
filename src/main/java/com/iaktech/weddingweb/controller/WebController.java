ead308fc9012fbceec539e552a98b69e27003ea8
Isaac Kusi <67994259+Isaac-AAK@users.noreply.github.com>
2023-07-04 11:42:27 -0400

    add  rsvp code and refactoring

--- src/main/java/com/iaktech/weddingweb/controller/WebController.java
+++ src/main/java/com/iaktech/weddingweb/controller/WebController.java
@@ -2,10 +2,10 @@ package com.iaktech.weddingweb.controller;
 
 
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.iaktech.weddingweb.model.Rsvp;
 import com.iaktech.weddingweb.service.Notification;
 import com.iaktech.weddingweb.service.RsvpService;
-
 import jakarta.mail.MessagingException;
 
 
@@ -26,12 +25,7 @@ import jakarta.mail.MessagingException;
 public class WebController {
 	@Autowired
 	private RsvpService rsvpService;
-	
-	@Autowired
-	private Notification notification;
-
-	private String name;
-	private String email;
+		
 	@RequestMapping(value = "/")
 	public String homePage(Model model) {
 		model.addAttribute("rsvpForm", new Rsvp());
@@ -46,19 +40,11 @@ public class WebController {
 	
 	@PostMapping("/rsvp-form")
 	public String  addRsvp(@Valid @ModelAttribute("rsvpForm") Rsvp addRsvp, BindingResult result) throws MessagingException {
-		if (result.hasErrors()) {
-			return "booking";
+		
+		if(rsvpService.addRsvp(addRsvp)) {
+			return "redirect:/";
 		}
-
-		 name=	addRsvp.getName();
-		 email=	addRsvp.getContact();
-        boolean checkName =name.matches("^[A-Za-z]+([\\ A-Za-z]+)*");
-			if (checkName){
-				addRsvp.getNumberOfGuest();
-				rsvpService.addRsvp(addRsvp);
-				notification.notificationTemplate(email,name);
-				return "redirect:/";
-			}
+		
 		return "redirect:/rsvp";
 	}
 }
--- src/main/java/com/iaktech/weddingweb/service/RsvpService.java
+++ src/main/java/com/iaktech/weddingweb/service/RsvpService.java
@@ -3,13 +3,22 @@ package com.iaktech.weddingweb.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.iaktech.weddingweb.model.Rsvp;
 import com.iaktech.weddingweb.repo.RsvpRepository;
 
+import jakarta.mail.MessagingException;
+
 @Service
 public class RsvpService {
+	
+	@Value("${rsvp.code}")
+	private String rsvpCode;
+	
+	@Autowired
+	private Notification notification;
 	
 	@Autowired
 	private RsvpRepository rsvpRepository;
@@ -18,8 +27,30 @@ public class RsvpService {
 		return rsvpRepository.findAll();
 	}
 
-	public Rsvp addRsvp( Rsvp rsvp) {
+	public boolean addRsvp( Rsvp rsvp) throws MessagingException {
 
-		return rsvpRepository.save(rsvp);
+		return addRsvps(rsvp);
+	}
+	
+	public List<Rsvp> findEmail(String email) {
+		return rsvpRepository.findByContact(email);
+	}
+	
+	private boolean addRsvps(Rsvp addRsvp) throws MessagingException {
+		String  name=	addRsvp.getName();
+		String email=	addRsvp.getContact();
+	 
+		var rsvp =  findEmail(email);
+		  if (rsvp.isEmpty()) {
+			  boolean checkName =name.matches("^[A-Za-z]+([\\ A-Za-z]+)*");
+			if (checkName && addRsvp.getRsvpCode().contentEquals(rsvpCode)){
+						addRsvp.getNumberOfGuest();
+						rsvpRepository.save(addRsvp);
+						notification.notificationTemplate(email,name);
+						return true;
+					}
+			
+		}
+ 	return false;
 	}
 }
